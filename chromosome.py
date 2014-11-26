bd0d309bd30c8d4f24e70b98fb27e998045aa5e1
Pavel Shamis <pashash@gmail.com>
2014-11-26 00:22:24 -0500

    Updating GA code.
    The code does not run and require some bugfixes

--- chromosome.py
+++ chromosome.py
@@ -3,15 +3,16 @@ import random
 import itertools
 
 class Chromosome:
-    def __init__(self, m, tabu_list=None):
+    def __init__(self, m, tabu_list=None, empty=False):
         ''' generate random chromosome from the model '''
         self.model=m
-        self.regenerate()
+        self.regenerate(empty)
         
-    def regenerate(self):
+    def regenerate(self, empty=False):
         self.map = model.Mapping(self.model)
-        for i in range(self.model.ntasks):
-            self.map.assign(i, random.randint(0, self.model.nmachines - 1))
+        if (not empty):
+            for i in range(self.model.ntasks):
+                self.map.assign(i, random.randint(0, self.model.nmachines - 1))
     
     def permutations(self):
         ''' generate permutation of task exchanges '''
@@ -38,3 +39,26 @@ class Chromosome:
     
     def above_50(self, ch):
         return self.map.similar(ch.map) / self.model.ntasks >= 0.5
+    
+    def apply_mutatation(self):
+        rand_task = random.randint(0, self.model.ntasks-1)
+        self.map.unassign(rand_task)
+        self.map.assign(rand_task, random.randint(0, self.model.nmachines - 1))
+
+
+def crossover(M, a, b):
+    assert a.model.ntasks ==  b.model.ntasks
+    assert a.model.nmachines ==  b.model.nmachines
+    assert M.nmachines ==  a.model.nmachines
+    assert M.ntasks ==  a.model.ntasks
+    
+    new = Chromosome(M, tabu_list=None, empty=True)
+    intersec = random.randint(0, M.model.ntasks-1)
+    
+    for t in range(intersec):
+        new.map.assign(t, a.model.machine(t))
+    
+    for t in range(intersec,M.model.ntasks):
+        new.map.assign(t, b.model.machine(t))
+        
+    return new
--- ga.py
+++ ga.py
@@ -1,23 +1,135 @@
 import model
 import argparse
 import itertools
-from chromosome import Chromosome 
+import numpy
+import random
+import bisect
+from chromosome import Chromosome , crossover
 
 parser = argparse.ArgumentParser('GA')
 parser.add_argument('-f', '--data-file', dest='data_file', required=True)
 args = parser.parse_args()
 
-M = model.Model(args.data_file)
-mapping = model.Mapping(M)
+##M = model.Model(args.data_file)
+##mapping = model.Mapping(M)
 
-P_SIZE=200 # population size
-TOTAL_ITER=1000 # total iterations
-MAX_NO_CHANGE=150 # no change in elite chromosome
+P_SIZE=200          # population size
+TOTAL_ITER=1000     # total iterations
+MAX_NO_CHANGE=150   # no change in elite chromosome
+CROSSOVER_PRB=0.6   # crossover probability 
+MUTATION_PRB=0.4    # mutation probability
 
 # Generate initial population. We seed it with one min-min value
-chrs=[]
-for _ in itertools.repeat(None, P_SIZE):
-    chrs.append(Chromosome(M))
-# Todo: Update the code to seed one entry from min-min
+def generate_new_population(M):
+    chrs=[]
+    for _ in itertools.repeat(None, P_SIZE):
+        chrs.append(Chromosome(M))
+    # Update the code to seed one entry from min-min
+    return chrs
 
-chrs[0].value()
+def calc_probability(chrs):
+    total = 0
+    result=[]
+    for ch in chrs:
+        total+=ch.value()
+    for ch in chrs:
+        # lowest value should have higher probability
+        # this normalization is broken
+        result.append(ch.value()/total)
+    
+    assert len(result) == len(chrs)    
+    return result
+
+def find_elite_index(prb):
+    return numpy.argmax(numpy.array(prb))
+
+def find_elite_ch(chrs, prb):
+    assert len(prb) == len(chrs)
+    return (chrs[find_elite_index(prb)], 
+            find_elite_index(prb)) 
+
+def select_with_probability(num, prb, chrs):
+    assert len(prb) == len(chrs)
+    acc_prb = [None]*len(prb)
+    acc_prb[0] = prb[0]
+    for i in range(1,len(prb)):
+        acc_prb[i]=acc_prb[i-1] + prb[i]
+    
+    # the rest of the selection code is based on 
+    # http://stackoverflow.com/questions/4113307/pythonic-way-to-select-list-elements-with-different-probability
+    result=[]
+    for _ in range(num):
+        x = random.random()
+        index = bisect.bisect(acc_prb, x)
+        result.append(prb[index])
+    
+    return result
+
+def select_for_crossover(chrs):
+    # Select even number of chromosome for crossover
+    result = []
+    for ch in chrs:
+        if random.random() <= CROSSOVER_PRB:
+            result.append(ch)
+    if (len(result)/2):
+        result.pop()
+    return result
+
+def mutate_chromosome(ch):
+    if random.random() <= MUTATION_PRB:
+        ch.apply_mutatation()  
+
+def mutate_population(chrs):
+    for ch in len(chrs):
+        if random.random() <= MUTATION_PRB:
+            ch.apply_mutatation()  
+
+def crossover_population(M, chrs):
+    c_ppl = select_for_crossover(chrs)
+    for _ in c_ppl:
+        a = c_ppl.pop()
+        b = c_ppl.pop()
+        c = crossover(M, a, b)
+        mutate_chromosome(c)
+        chrs.append(c)
+
+def ga(M):
+    elite_count = 0
+    elite_value = -1
+    ppl = generate_new_population(M)
+    
+    for i in range(TOTAL_ITER):
+        print "Iteration ", i
+        
+        pb = calc_probability(ppl)
+        
+        # select elite and do the checks        
+        elite, index = find_elite_ch(ppl,pb)
+        
+        if(elite.value() == elite_value):
+            elite_count += 1
+        else:
+            if elite.value() > elite_value and -1 != elite_value:
+                print "Bug ! new elite > old elite", elite.value, elite_value
+            elite_value = elite.value()
+            elite_count = 0
+            
+        if elite_count == MAX_NO_CHANGE:
+            return elite
+        
+        ppl.pop(index)            
+        selection = select_with_probability(P_SIZE-1, 
+                                            calc_probability(ppl), 
+                                            ppl)
+        selection.append(elite)
+        # selection is done
+        crossover_population(M, selection)
+        ppl = selection
+    
+    return elite
+        
+M = model.Model(args.data_file)
+best_c = ga(M)
+
+print "Mapping: ", best_c.map
+print "Value: ", best_c.value()
